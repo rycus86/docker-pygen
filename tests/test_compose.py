3d6ca69b5639db2f1cde33033c3201250caec01b
Viktor Adam <rycus86@gmail.com>
2017-07-06 07:17:32 +0000

    [WIP] Basic docker-compose sample

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -1,3 +1,6 @@
+import os
+import time
+import json
 import unittest
 from compose.config.config import ConfigFile, ConfigDetails
 from compose.config.config import load as load_config
@@ -7,25 +10,38 @@ from unittest_helper import relative_path
 import pygen
 
 
-@unittest.skip
+@unittest.skipIf(os.environ.get('TEST_IMAGE'), 'Skipping on non-x86 architectures')
 class DockerComposeTests(unittest.TestCase):
+    project = None
+
     def setUp(self):
         self.app = pygen.PyGen(template=relative_path('templates/mockserver.conf.json'))
+
+    def tearDown(self):
+        if self.project:
+            self.project.down(False, True, True)
 
     def test_compose(self):
         config = ConfigFile.from_filename(relative_path('compose/sample.yml'))
         details = ConfigDetails(relative_path('compose'), [config])
-        project = Project.from_config('pygen-test', load_config(details), self.app.api.client.api)
+        self.project = Project.from_config('pygen-test', load_config(details), self.app.api.client.api)
+        
+        self.project.up(detached=True)
+        
+        content = self.app.generate()
 
-        project.up(detached=False)
+        print(content)
+        
+        parsed = json.loads(content)
+        
+        self.assertIn('server', parsed)
+        self.assertIn('proxy', parsed['server'])
 
-        try:
-            import time
-            for _ in range(10):
-                print(self.app.api.list())
-                time.sleep(0.5)
+        hosts = parsed['server']['proxy']
 
-            project.down()
+        self.assertEquals(len(hosts), 2)
 
-        except:
-            pass
+        for host in hosts:
+            self.assertIn('host', host)
+            self.assertIn(host['host'], ('api.sample.com', 'www.sample.com'))
+
