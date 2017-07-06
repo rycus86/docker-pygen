c8c876aaed47cace55872bcfa3510fa6d91a24a9
Viktor Adam <rycus86@gmail.com>
2017-07-06 17:51:42 +0000

    docker-compose with tests

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -10,7 +10,7 @@ from unittest_helper import relative_path
 import pygen
 
 
-@unittest.skipIf(os.environ.get('TEST_IMAGE'), 'Skipping on non-x86 architectures')
+@unittest.skipUnless(os.environ.get('COMPOSE_TESTS'), 'Skipping docker-compose tests')
 class DockerComposeTests(unittest.TestCase):
     project = None
 
@@ -21,7 +21,7 @@ class DockerComposeTests(unittest.TestCase):
         if self.project:
             self.project.down(False, True, True)
 
-    def test_compose(self):
+    def test_compose_project(self):
         config = ConfigFile.from_filename(relative_path('compose/sample.yml'))
         details = ConfigDetails(relative_path('compose'), [config])
         self.project = Project.from_config('pygen-test', load_config(details), self.app.api.client.api)
@@ -30,8 +30,6 @@ class DockerComposeTests(unittest.TestCase):
         
         content = self.app.generate()
 
-        print(content)
-        
         parsed = json.loads(content)
         
         self.assertIn('server', parsed)
@@ -44,4 +42,110 @@ class DockerComposeTests(unittest.TestCase):
         for host in hosts:
             self.assertIn('host', host)
             self.assertIn(host['host'], ('api.sample.com', 'www.sample.com'))
+            
+            self.assertIn('backends', host)
+
+            backends = host['backends']
+
+            if host['host'] == 'www.sample.com':
+                self.assertEquals(1, len(backends))
+                
+                backend = backends[0]
+                
+                self.assertIn('context', backend)
+                self.assertEquals(backend.get('context'), '/')
+                self.assertIn('servers', backend)
+                self.assertEquals(1, len(backend['servers']))
+
+                c_web = self.project.get_service('web').get_container().inspect()
+                c_web_ip = next(iter(c_web['NetworkSettings']['Networks'].values())).get('IPAddress')
+
+                self.assertEquals('http://%s:8080/pygen-test_web_1' % c_web_ip, backend['servers'][0])
+
+            else:
+                self.assertEquals(2, len(backends))
+
+                for backend in backends:
+                    self.assertIn('context', backend)
+                    self.assertIn(backend.get('context'), ('/rest', '/stream'))
+                    
+                    self.assertIn('servers', backend)
+                    self.assertEquals(1, len(backend['servers']))
+
+                    if backend['context'] == '/rest':
+                        c_app_a = self.project.get_service('app-a').get_container().inspect()
+                        c_app_a_ip = next(iter(c_app_a['NetworkSettings']['Networks'].values())).get('IPAddress')
+
+                        self.assertEquals('http://%s:9001/pygen-test_app-a_1' % c_app_a_ip, backend['servers'][0])
+
+                    else:
+                        c_app_b = self.project.get_service('app-b').get_container().inspect()
+                        c_app_b_ip = next(iter(c_app_b['NetworkSettings']['Networks'].values())).get('IPAddress')
+
+                        self.assertEquals('http://%s:9001/pygen-test_app-b_1' % c_app_b_ip, backend['servers'][0])
+
+    def test_scale_update(self):
+        config = ConfigFile.from_filename(relative_path('compose/sample.yml'))
+        details = ConfigDetails(relative_path('compose'), [config])
+        self.project = Project.from_config('pygen-test', load_config(details), self.app.api.client.api)
+        
+        self.project.up(detached=True)
+        
+        content = self.app.generate()
+
+        parsed = json.loads(content)
+
+        self.assertIn('server', parsed)
+        self.assertIn('proxy', parsed['server'])
+
+        hosts = parsed['server']['proxy']
+
+        self.assertEquals(len(hosts), 2)
+
+        for host in hosts:
+            self.assertIn('host', host)
+            self.assertIn(host['host'], ('api.sample.com', 'www.sample.com'))
+            
+            if host['host'] == 'www.sample.com':
+                self.assertIn('backends', host)
+
+                backends = host['backends']
+
+                self.assertEqual(len(backends), 1)
+
+                servers = backends[0].get('servers')
+
+                self.assertIsNotNone(servers)
+                self.assertEqual(len(servers), 1)
+
+        service_web = self.project.get_service('web')
+        
+        service_web.scale(3)
+
+        content = self.app.generate()
+        
+        parsed = json.loads(content)
+
+        self.assertIn('server', parsed)
+        self.assertIn('proxy', parsed['server'])
+
+        hosts = parsed['server']['proxy']
+
+        self.assertEquals(len(hosts), 2)
+
+        for host in hosts:
+            self.assertIn('host', host)
+            self.assertIn(host['host'], ('api.sample.com', 'www.sample.com'))
+            
+            if host['host'] == 'www.sample.com':
+                self.assertIn('backends', host)
+
+                backends = host['backends']
+
+                self.assertEqual(len(backends), 1)
+
+                servers = backends[0].get('servers')
+
+                self.assertIsNotNone(servers)
+                self.assertEqual(len(servers), 3)
 
