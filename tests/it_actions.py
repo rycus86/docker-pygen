0c1aab5864f02b9a62ee885ee6c7114d63a3c907
Viktor Adam <rycus86@gmail.com>
2017-10-25 18:09:10 +0000

    [WIP] Integration tests [skip ci]

--- tests/it_actions.py
+++ tests/it_actions.py
@@ -1,3 +1,5 @@
+import time
+
 from integrationtest_helper import BaseDockerIntegrationTest
 
 
@@ -53,24 +55,87 @@ class ActionIntegrationTest(BaseDockerIntegrationTest):
         self.assertIn('Signalled', newer_logs)
 
     def test_restart_service(self):
-        command = self.init_swarm()
+        join_command = self.init_swarm()
 
         with self.with_dind_container() as second_dind:
             
             self.prepare_images('alpine', client=self.dind_client(second_dind))
 
-            second_dind.exec_run(command)
+            second_dind.exec_run(join_command)
 
             service = self.remote_client.services.create('alpine',
                                                          name='target-svc',
                                                          mode='global',
                                                          command='sh -c "date +%s ; sleep 3600"',
                                                          stop_grace_period=1)
+            
+            for _ in range(10):
+                if all(task['Status']['State'] == 'running' for task in service.tasks()):
+                    break
 
-            import time
-            time.sleep(2)
+                time.sleep(0.5)
 
-            print 'logs:'
-            for line in service.logs(stdout=True):
-                print line
+            initial_logs = set(line for line in service.logs(stdout=True) if line.strip())
+
+            command = [
+                '--template #ok',
+                '--restart target-svc',
+                '--one-shot'
+            ]
+
+            self.remote_client.containers.run('pygen-build', command=' '.join(command), remove=True,
+                                              volumes=['/var/run/docker.sock:/var/run/docker.sock:ro'])
+
+            for _ in range(60):
+                if len(service.tasks()) >= 4:
+                    if all(task['Status']['State'] == 'running'
+                           for task in service.tasks(filters={'desired-state': 'running'})):
+                        break
+
+                time.sleep(0.5)
+
+            newer_logs = set(line for line in service.logs(stdout=True) if line.strip())
+
+            self.assertNotEqual(newer_logs, initial_logs)
+
+    def test_signal_service(self):
+        join_command = self.init_swarm()
+
+        with self.with_dind_container() as second_dind:
+            
+            self.prepare_images('alpine', 'pygen-build', client=self.dind_client(second_dind))
+
+            second_dind.exec_run(join_command)
+
+            command = 'sh -c "echo \'Starting...\'; trap \\"echo \'Signalled \'$(hostname)\\" SIGHUP && read"'
+            service = self.remote_client.services.create('alpine',
+                                                         name='target-svc',
+                                                         mode='global',
+                                                         command=command,
+                                                         tty=True)
+            
+            for _ in range(10):
+                if all(task['Status']['State'] == 'running' for task in service.tasks()):
+                    break
+
+                time.sleep(0.5)
+
+            initial_logs = list(''.join(line for line in service.logs(stdout=True)).split('\n'))
+            
+            # TODO start workers
+
+            command = [
+                '--template #ok',
+                '--signal target-svc HUP',
+                '--swarm-manager',
+                '--workers tasks.pygen-worker',
+                '--one-shot'
+            ]
+
+            self.remote_client.containers.run('pygen-build', command=' '.join(command), remove=True,
+                                              volumes=['/var/run/docker.sock:/var/run/docker.sock:ro'])
+            
+            newer_logs = list(''.join(line for line in service.logs(stdout=True)).split('\n'))
+
+            self.assertNotEqual(tuple(sorted(newer_logs)), tuple(sorted(initial_logs)))
 
